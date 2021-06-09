45e08155ceb598c57f159e2c050209742971cb6f
hirose <hirosehirotaka@localhost>
2021-06-09 22:11:20 +0900

    add NamedParameterJdbcTemplate sample

--- SpringSample/src/main/java/com/example/demo/login/domain/service/UserService.java
+++ SpringSample/src/main/java/com/example/demo/login/domain/service/UserService.java
@@ -1,9 +1,15 @@
 package com.example.demo.login.domain.service;
 
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.login.domain.model.User;
@@ -13,7 +19,7 @@ import com.example.demo.login.domain.repository.UserDao;
 public class UserService {
 
 	@Autowired
-	@Qualifier("UserDaoJdbcImpl4") // specify Bean
+	@Qualifier("UserDaoNamedJdbcImpl") // specify Bean
 	UserDao dao;
 
 	public boolean insert(User user) {
@@ -64,5 +70,18 @@ public class UserService {
 		}
 
 		return result;
+	}
+
+	public void userCsvOut() throws DataAccessException {
+		dao.userCsvOut();
+	}
+
+	public byte[] getFile(String fileName) throws IOException {
+		FileSystem fs = FileSystems.getDefault();
+		Path p = fs.getPath(fileName);
+		// ファイルをバイト配列に変換
+		byte[] bytes = Files.readAllBytes(p);
+
+		return bytes;
 	}
 }
