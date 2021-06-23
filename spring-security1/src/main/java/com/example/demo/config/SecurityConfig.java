8f323d14b4eb8475a756e03a1a095d5ceeecd57a
hirose <hirosehirotaka@localhost>
2021-06-23 22:41:24 +0900

    spring security sample

---
+++ spring-security1/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity // Spring Securityの機能を有効化
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		// パスワードの暗号化用に,bcryptを使用します
+		return new BCryptPasswordEncoder();
+	}
+
+	// configure(http)・・httpリクエストの設定
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				// 認証リクエストの設定
+				.authorizeRequests()
+				// 認証の必要があるように設定
+				.anyRequest().authenticated().and()
+				// フォームベース認証の設定
+				.formLogin();
+	}
+
+	// configure(auth)・・ユーザの設定
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+				// メモリ内認証を設定
+				.inMemoryAuthentication()
+				// "user"を追加
+				.withUser("user")
+				// "password"をBCryptで暗号化
+				.password(passwordEncoder().encode("password"))
+				// 権限(ロール)を設定
+				.authorities("ROLE_USER");
+	}
+
+}
